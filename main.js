94dc3f8ddb3d7a949ef1a7659367b96f39bc3f86
doped-semiconductor <getsreya@gmail.com>
2020-06-25 00:29:03 +0530

    neo class

--- main.js
+++ main.js
@@ -3,20 +3,21 @@ var express = require('express')
 var bodyParser = require('body-parser')
 const fs = require('fs');
 var ke = require('./keywordextract')
+var njq = require('./neo4j-query')
 app = express()
 app.use(bodyParser.urlencoded({
   extended: true
 }))
 app.use(bodyParser.json())
-app.post('/', (request, response)=>{
-    postHandler(request, response)
+app.post('/', async (request, response)=>{
+    await postHandler(request, response)
 });  
 
 app.listen(5000);
 console.log("server on 5000");
 
 async function postHandler(request, response){
-    console.log("req body:",request.body);  
+    //console.log("req body:",request.body);  
     /** check if content received */    
     if (Object.keys(request.body.data).length === 0) 
     {
@@ -24,7 +25,8 @@ async function postHandler(request, response){
         response.send({data:'not recieved'});
     }
     else{
-        //IMPORT BOOKMARKS FROM CLIENT
+        
+        //IMPORT BOOKMARKS FROM CLIENT - works
         if (request.body.instruction == 'import'){
             for (let i in request.body.data){ 
                 request.body.data[i].readlater = false
@@ -39,8 +41,8 @@ async function postHandler(request, response){
                             keys.forEach((el) => {
                                 el.id = request.body.data[i].id
                                 //console.log('el',el)
-                                addKeys(el) 
-                                fs.appendFile('neologs.txt', JSON.stringify(el)+"\n", function (err) {if (err) throw err;});                   
+                                var neo = new njq.neo4jQueries()                
+                                var res = await neo.addKeys(el)  
                             })
                         }
                         else{
@@ -51,6 +53,18 @@ async function postHandler(request, response){
             }
             addRelations()
             response.send({data:'received',n :Object.keys(request.body.data).length});
+        }
+        //GET RECENTS
+        else if(request.body.instruction == 'recent'){
+            if(!request.body.data.lim){
+                console.log('lim not recieved')
+                response.send({data:'not received'});
+            }
+            else{
+                var neo = new njq.neo4jQueries()
+                var res = await neo.RecentBookmarks(request.body.data.lim)
+                response.send({data:'recieved',output:res})
+            }
         }
         //GENERATE TAGS - INCOMPLETE
         else if(request.body.instruction == 'tags'){
@@ -67,7 +81,7 @@ async function postHandler(request, response){
                 })
             }                
         }
-        //SEARCH
+        //SEARCH -  works
         else if(request.body.instruction == 'search'){
             //SEARCH BY TAG
             console.log('search req recieved')
@@ -81,131 +95,54 @@ async function postHandler(request, response){
                 else{
                     request.body.data.tag = request.body.data.tag.toLowerCase()
                     console.log('tag recieved: ',request.body.data.tag)
-                    //query with the tag
-                    var driver = neo4j.driver(
-                        'bolt://localhost:7687',
-                        neo4j.auth.basic('neo4j', 'bookmarks')  
-                    )
-                    var session = driver.session()
-                    try {    
-                        console.log('key: ',request.body.data.tag)
-                        const result = await session.writeTransaction(tx =>
-                            
-                            tx.run('MATCH (key: Keyword)-[keyof:KEYOF]->(book:Bookmark) WHERE key.phrase contains $tag return book'
-                            ,{tag:request.body.data.tag})
-                            .then(res => {
-                                var op=[]
-                                res.records.forEach(record=>{
-                                    var temp = {
-                                        title: record.get(0).properties.title,
-                                        url: record.get(0).properties.url
-                                    }
-                                    //console.log('r',temp)
-                                    op.push(temp)
-                                })
-                                console.log('res',res.record.get(0).properties)
-                                response.send({data:'received','output':op});                                    
-                            })
-                          .catch(err =>{console.log(err.message)})
-                        )      
-                    }     
-                    finally { await session.close() }  
-                    await driver.close()
-
-                    //send response with results
+                    //query with the tag    
+                    var neo = new njq.neo4jQueries()                
+                    var res = await neo.SearchByTag(request.body.data.tag)
+                    response.send({data:'recieved',output:res})
                 }
 
             }
-            else if (request.body.type=="byDomain"){}
-            else if (request.body.type=="byTitle"){}
+            //SEARCH BY DOMAIN
+            else if (request.body.data.type=="byDomain"){
+                if(!request.body.data.domain){
+                    console.log('domain not recieved')
+                    response.send({data:'not received'});
+                }
+                else{
+                    console.log('domain recieved: ',request.body.data.domain)
+                    //query with the tag
+                    var neo = new njq.neo4jQueries()
+                    var res = await neo.SearchByDomain(request.body.data.domain)
+                    console.log('res to send',res)
+                    //send response with results
+                    response.send({data:'recieved',output:res})
+                }
+            }
+            //SEARCH BY TITLE
+            else if (request.body.data.type=="byTitle"){
+                if(!request.body.data.title){
+                    console.log('title not recieved')
+                    response.send({data:'not received'});
+                }
+                else{
+                    console.log('title recieved: ',request.body.data.title)
+                    //query with the tag
+                    var neo = new njq.neo4jQueries()
+                    var res = await neo.SearchByTitle(request.body.data.title)
+                    //console.log('res to send',res)
+                    //send response with results
+                    response.send({data:'recieved',output:res})
+                }
+            }
         }
         //GIVE FILE NAVIGATION
+        else if(request.body.instruction == 'files'){}
         
         //DELETE FILE/BOOKMARK
+        else if(request.body.instruction == 'delete'){}
 
-        //SIMILAR BOOKMARKS
+        //SIMILAR TAGS
+        else if(request.body.instruction == 'similar'){}
     };
-}
-
-////FUNCTION - ADD KEYS NODE TO DB
-async function addKeys(keys){
-    var driver = neo4j.driver(
-        'bolt://localhost:7687',
-        neo4j.auth.basic('neo4j', 'bookmarks')  
-    )
-    var session = driver.session()
-    try {
-        keys.term = keys.term.toLowerCase()    
-        const result0 = await session.writeTransaction(tx =>
-            tx.run(`merge(n:Keyword{phrase:$term})`,
-            {term: keys.term})
-            .then(res => {console.log('added: KEYWORD - ',keys.term )})
-            .catch(err =>{console.log(err.message)})
-        )   
-        const result = await session.writeTransaction(tx =>
-            tx.run(`match(n:Keyword),(b:Bookmark) where n.phrase=$term and b.id=$id merge (n)-[r:KEYOF {tf: $tf}]->(b)`,
-            keys)
-          .then(res => {console.log('added: KEYWORD RELATIONSHIP - ',keys.id,keys.term)})
-          .catch(err =>{console.log(err.message)})
-        )      
-    }     
-    finally { await session.close() }  
-    await driver.close()
-
-}
-
-////FUNCTION - ADD RELATIONS NODE TO DB
-async function addRelations(){
-    var driver = neo4j.driver(
-        'bolt://localhost:7687',
-        neo4j.auth.basic('neo4j', 'bookmarks')  
-    )
-    var session = driver.session()
-    try {    
-        const result = await session.writeTransaction(tx =>
-          tx.run('match (n),(p:Folder) where n.parent = p.id merge (n)-[r:CHILDOF]->(p) return n')
-          .then(res => {console.log('added: RELATIONSHIPS')})
-          .catch(err =>{console.log(err.message)})
-        )      
-    }     
-    finally { await session.close() }  
-    await driver.close()
-}
-
-//FUNCTION - ADD BOOKMARK NODE TO DB
-async function addBookmark(bookmark){
-    var driver = neo4j.driver(
-        'bolt://localhost:7687',
-        neo4j.auth.basic('neo4j', 'bookmarks')  
-    )
-    var session = driver.session()
-    try {    
-        const result = await session.writeTransaction(tx =>
-          tx.run('MERGE (b: Bookmark {id:$id,title:$title,index:$index,url:$url,parent:$parent,date:$date,visits:$visits,rl:$readlater}) return b',bookmark)
-          .then(res => {console.log('added: ',bookmark.title)})
-          .catch(err =>{console.log(err.message)})
-        )      
-    }     
-    finally { await session.close() }  
-    await driver.close()
-}
-
-//FUNCTION - ADD FOLDER NODE TO DB
-async function addFolder(bookmark){
-    //
-    var driver = neo4j.driver(
-        'bolt://localhost:7687',
-        neo4j.auth.basic('neo4j', 'bookmarks')  
-    )
-    var session = driver.session()
-    try {    
-        const result = await session.writeTransaction(tx =>
-          tx.run('MERGE (b: Folder {id:$id,title:$title,index:$index,parent:$parent,date:$date}) return b',bookmark)
-          .then(res => {console.log('added: ',bookmark.title)})
-          .catch(err =>{console.log(err.message)})
-        )       
-    }     
-    finally { await session.close() }  
-    await driver.close()
 }
 
---
+++ neo4j-query.js
@@ -0,0 +1,189 @@
+neo4j = require('neo4j-driver')
+class neo4jQueries{
+    constructor(){
+        this.driver = null
+        this.session = null
+        this.result = null        
+    }
+    ////FUNCTION -SEARCH RECENTS
+    async RecentBookmarks(n){
+        var output = [];
+        var driver = neo4j.driver(
+            'bolt://localhost:7687',
+            neo4j.auth.basic('neo4j', 'bookmarks')  
+        )
+        var session = driver.session()
+        try {    
+            const result = await session.writeTransaction(tx =>
+              tx.run('MATCH(b:Bookmark) RETURN b ORDER BY b.date DESC LIMIT $n',{n: n})
+              .then(res => {
+                  res.records.forEach(record=>{
+                      output.push(record.get(0).properties)})
+                  })
+              .catch(err =>{console.log(err.message)})
+            )      
+        }     
+        finally { await session.close() }  
+        await driver.close()
+        return output
+    }
+    ////FUNCTION -SEARCH BY TITLE
+    async SearchByTitle(title){
+        var op = []
+        var driver = neo4j.driver(
+            'bolt://localhost:7687',
+            neo4j.auth.basic('neo4j', 'bookmarks')  
+        )
+        var session = driver.session()
+        try {    
+            const result = await session.writeTransaction(tx =>
+              tx.run('MATCH (b: Bookmark) WHERE b.title CONTAINS $title SET b.date=date() return b',{title:title})
+              .then(res => {
+                  res.records.forEach(record=>{
+                      //console.log(record.get(0).properties)
+                      op.push(record.get(0).properties)})
+                 //console.log(op)
+                  })
+              .catch(err =>{console.log(err.message)})
+            )      
+        }     
+        finally { await session.close() }  
+        await driver.close()
+        return op
+    }
+    ////FUNCTION -SEARCH BY DOMAIN
+    async SearchByDomain(domain){
+        var op = []
+        var driver = neo4j.driver(
+            'bolt://localhost:7687',
+            neo4j.auth.basic('neo4j', 'bookmarks')  
+        )
+        var session = driver.session()
+        try {    
+            const result = await session.writeTransaction(tx =>
+                tx.run('MATCH (b: Bookmark) WHERE b.url CONTAINS $domain SET b.date=date() return b',{domain:domain})
+                .then(res => {
+                    res.records.forEach(record=>{
+                        op.push(record.get(0).properties)})
+                        //console.log(op)
+                    })
+                .catch(err =>{console.log(err.message)})
+            )      
+        }     
+        finally { await session.close() }  
+        await driver.close()
+        return op
+    }
+    ////FUNCTION -SEARCH BY TAG
+    async SearchByTag(tag){
+        var op = [];
+        var driver = neo4j.driver(
+            'bolt://localhost:7687',
+            neo4j.auth.basic('neo4j', 'bookmarks')  
+        )
+        var session = driver.session()
+        try {    
+            const result = await session.writeTransaction(tx =>
+                tx.run('MATCH (key: Keyword)-[keyof:KEYOF]->(book:Bookmark) WHERE key.phrase CONTAINS $tag return book',{tag:tag})
+                .then(res => {
+                    res.records.forEach(record=>{
+                        op.push(record.get(0).properties)})                
+                })
+                .catch(err =>{console.log(err.message)})
+            )      
+        }     
+        finally { await session.close() }  
+        await driver.close()
+        return op
+    }
+    ////FUNCTION - ADD KEYS NODE TO DB
+    async addKeys(keys){
+        var driver = neo4j.driver(
+            'bolt://localhost:7687',
+            neo4j.auth.basic('neo4j', 'bookmarks')  
+        )
+        var session = driver.session()
+        try {
+            keys.term = keys.term.toLowerCase()    
+            const result0 = await session.writeTransaction(tx =>
+                tx.run(`merge(n:Keyword{phrase:$term})`,
+                {term: keys.term})
+                .then(res => {console.log('added: KEYWORD - ',keys.term )})
+                .catch(err =>{console.log(err.message)})
+            )   
+            const result = await session.writeTransaction(tx =>
+                tx.run(`match(n:Keyword),(b:Bookmark) where n.phrase=$term and b.id=$id merge (n)-[r:KEYOF {tf: $tf}]->(b)`,
+                keys)
+            .then(res => {console.log('added: KEYWORD RELATIONSHIP - ',keys.id,keys.term)})
+            .catch(err =>{console.log(err.message)})
+            )      
+        }     
+        finally { await session.close() }  
+        await driver.close()
+
+    }
+    ////FUNCTION - ADD RELATIONS NODE TO DB
+    async addRelations(){
+        var driver = neo4j.driver(
+            'bolt://localhost:7687',
+            neo4j.auth.basic('neo4j', 'bookmarks')  
+        )
+        var session = driver.session()
+        try {    
+            const result = await session.writeTransaction(tx =>
+            tx.run('match (n),(p:Folder) where n.parent = p.id merge (n)-[r:CHILDOF]->(p) return n')
+            .then(res => {console.log('added: RELATIONSHIPS')})
+            .catch(err =>{console.log(err.message)})
+            )      
+        }     
+        finally { await session.close() }  
+        await driver.close()
+    }
+    //FUNCTION - ADD BOOKMARK NODE TO DB
+    async addBookmark(bookmark){
+        var driver = neo4j.driver(
+            'bolt://localhost:7687',
+            neo4j.auth.basic('neo4j', 'bookmarks')  
+        )
+        var session = driver.session()
+        try {    
+            const result = await session.writeTransaction(tx =>
+            tx.run('MERGE (b: Bookmark {id:$id,title:$title,index:$index,url:$url,parent:$parent,date:$date,visits:$visits,rl:$readlater}) return b',bookmark)
+            .then(res => {console.log('added: ',bookmark.title)})
+            .catch(err =>{console.log(err.message)})
+            )      
+        }     
+        finally { await session.close() }  
+        await driver.close()
+    }
+    //FUNCTION - ADD FOLDER NODE TO DB
+    async addFolder(bookmark){
+        //
+        var driver = neo4j.driver(
+            'bolt://localhost:7687',
+            neo4j.auth.basic('neo4j', 'bookmarks')  
+        )
+        var session = driver.session()
+        try {    
+            const result = await session.writeTransaction(tx =>
+            tx.run('MERGE (b: Folder {id:$id,title:$title,index:$index,parent:$parent,date:$date}) return b',bookmark)
+            .then(res => {console.log('added: ',bookmark.title)})
+            .catch(err =>{console.log(err.message)})
+            )       
+        }     
+        finally { await session.close() }  
+        await driver.close()
+    }
+}
+
+exports.neo4jQueries = neo4jQueries
+
+
+/** EXP 
+async function main(){
+    var p = new neo4jQueries()
+    var o = await p.RecentBookmarks()
+    console.log(o)
+}
+
+main()*/
